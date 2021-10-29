e947844718a19e4fdaf24eb1c96846311734580c
saikm22000 <saikiran.m00@gmail.com>
2021-10-28 22:41:34 -0500

    Remove make files

--- build/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/skm2019/Documents/CS393R/cs393r_starter/build/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
